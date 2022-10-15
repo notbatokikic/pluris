adbbd6f027595bedf4a8bf30299549cfc4244f9f
notbatokikic <bato.kikic@gmail.com>
2022-10-15 17:33:05 +0200

    finalised mobile design

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+navbarLinks.classList.toggle('active');
+})
+
+toggleButton.addEventListener('click', () => {
+    toggleButton.classList.toggle('is-active');
+    })
